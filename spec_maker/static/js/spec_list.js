73d0a5c1b65f791293dc150dc17337e3d5293227
carl_huang <carl_huang@gemteks.com>
2015-01-05 16:33:41 +0800

    Add rebuild and delete request

--- spec_maker/static/js/spec_list.js
+++ spec_maker/static/js/spec_list.js
@@ -26,4 +26,64 @@ $(function () {
 
         }
     });
+
+
+    function getSelectedNodesAndAjax(action, ajaxCallback) {
+        var data, specs, $specSelectChecked, i, len;
+        $specSelectChecked = $('input[name="spec-select"]').filter(':checked');
+        specs = [];
+        for (i = 0, len = $specSelectChecked.length; i < len; i += 1) {
+            specs.push($specSelectChecked[i].value);
+        }
+        data = {
+            action: action,
+            specs: specs
+        };
+        console.log(data);
+        $.ajax({
+            url: '/specs/',
+            type: 'POST',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data),
+            dataType: 'json',
+            success: ajaxCallback,
+            complete: function (jqXHR, textStatus) {
+                console.log(jqXHR);
+                console.log(textStatus);
+                // ladda.stop();
+            },
+        });
+    }
+
+
+    $('#rebuild').click(function () {
+        getSelectedNodesAndAjax('rebuild', function (result) {
+            console.log('Rebuild spec trees completed');
+            if (result.error === undefined) {
+                alert('The response format is wrong!');
+                return -1;
+            }
+            if (result.error === 0) {
+                alert('The specs are rebuilt successfully!');
+            } else {
+                alert('The spec rebuild is failed!');
+            }
+        });
+    });
+
+
+    $('#delete').click(function () {
+        getSelectedNodesAndAjax('delete', function (result) {
+            console.log('Delete specs completed');
+            if (result.error === undefined) {
+                alert('The response format is wrong!');
+                return -1;
+            }
+            if (result.error === 0) {
+                alert('The specs are deleted successfully!');
+            } else {
+                alert('The specs deletion is failed!');
+            }
+        });
+    });
 });
