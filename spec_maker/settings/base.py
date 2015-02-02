0bcaa372bae41c5fc9f8e8df5433520db6afa1c7
carl_huang <carl_huang@gemteks.com>
2015-02-02 17:55:36 +0800

    Do not use DB by default

--- spec_maker/settings/base.py
+++ spec_maker/settings/base.py
@@ -18,14 +18,14 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'spec_maker',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'spec_maker',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': '',
+    #     'PORT': '',
+    # }
 }
 
 # Local time zone for this installation. Choices can be found here:
